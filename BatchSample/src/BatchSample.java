c4a97bc22b680573099b3df7080714287673357e
Hoseop <hoseob.ji@gmail.com>
2019-07-10 00:42:56 +0900

    [add] Get the Gap of the days
    unstable need +a

--- BatchSample/src/BatchSample.java
+++ BatchSample/src/BatchSample.java
@@ -7,6 +7,7 @@
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -32,17 +33,21 @@ public class BatchSample {
 		Logger logger = Logger.getLogger(BatchSample.class.getName());
 		
 		Connection conn = null;
+		ArrayList<Timestamp> procTimeList = new ArrayList<>();
 		try {
 			Class.forName(Driver);		
 			conn = DriverManager.getConnection(url,USER,pass);
 			PreparedStatement st = conn.prepareStatement(selectSql);
 			ResultSet rs =  st.executeQuery();
 			
+			
 			while (rs.next()) {
-				System.out.println(rs.getTimestamp(1));
+				procTimeList.add(rs.getTimestamp(1));
 				System.out.println(editType(rs.getString(2)).getAction());
 				System.out.println(editType(rs.getString(2)).getIo_cd());
 				System.out.println(editType(rs.getString(2)).getUsr_id());
+				System.out.println(editType(rs.getString(2)).getProcess_id());
+				System.out.println(editType(rs.getString(2)).getPower());
 			}
 			
 			
@@ -52,6 +57,11 @@ public class BatchSample {
 			e.printStackTrace();
 		} finally {
 		}	
+		
+		System.out.println(procTimeList.get(0));
+		System.out.println(procTimeList.get(1));
+		timeTaken(procTimeList.get(0), procTimeList.get(1));
+		
 	}
 	
 	public static Work_Log editType(String message) {
@@ -77,9 +87,44 @@ public class BatchSample {
 		wLog.setAction(msgString[0]);
 		wLog.setIo_cd(msgString[1]);
 		wLog.setUsr_id(msgString[2]);*/
-		wLog.setAction(wColList.get(0));
-		wLog.setIo_cd(wColList.get(1));
-		wLog.setUsr_id(wColList.get(2));
+		wLog.setAction(wColList.get(0).substring(wColList.get(0).indexOf(":") +1));
+		wLog.setIo_cd(wColList.get(1).substring(wColList.get(1).indexOf(":") +1));
+		wLog.setUsr_id(wColList.get(2).substring(wColList.get(2).indexOf(":") +1));
+		
+		wLog.setProcess_id(wColList.get(3).substring(wColList.get(3).indexOf(":") +1, wColList.get(3).indexOf(" ")));
+		wLog.setPower(wColList.get(3).substring(wColList.get(3).lastIndexOf(":")+1));
+		
 		return wLog;
 	}
+	
+	
+	public static String timeTaken(Timestamp startStamp, Timestamp endStamp) {
+		
+
+		 
+		
+		long diff =  endStamp.getTime() - startStamp.getTime();
+		
+		
+		long diffDays = diff / (24*60*60*1000);
+		long diffHours = diff / (60*60*1000);
+		long diffMins =  diff / (60*1000);		
+		long sec = diff/1000;
+		System.out.println("millistake : " + diff);
+		System.out.println("sectaken : " + sec);
+		System.out.println("minstaken : " + diffMins);
+		System.out.println("hourstaken : " + diffHours);
+		System.out.println("daystaken : " + diffDays);
+		
+		
+		
+		Date d = new Date(sec);
+		
+		Calendar cal = Calendar.getInstance();		
+		cal.setTimeInMillis(diff);	
+		System.out.println(cal.getTime());
+		
+		return "";
+	}
+	
 }
--- BatchSample/src/Work_Log.java
+++ BatchSample/src/Work_Log.java
@@ -3,7 +3,7 @@ import java.sql.Timestamp;
 public class Work_Log {
 
 	private Timestamp proc_time;
-	private String action, io_cd, usr_id;
+	private String action, io_cd, usr_id, process_id, power;
 	public Timestamp getProc_time() {
 		return proc_time;
 	}
@@ -28,17 +28,32 @@ public class Work_Log {
 	public void setUsr_id(String usr_id) {
 		this.usr_id = usr_id;
 	}
-	public Work_Log(Timestamp proc_time, String action, String io_cd, String usr_id) {
+	
+	public String getProcess_id() {
+		return process_id;
+	}
+	public void setProcess_id(String process_id) {
+		this.process_id = process_id;
+	}
+	public String getPower() {
+		return power;
+	}
+	public void setPower(String power) {
+		this.power = power;
+	}
+	public Work_Log(Timestamp proc_time, String action, String io_cd, String usr_id, String process_id, String power) {
 		super();
 		this.proc_time = proc_time;
 		this.action = action;
 		this.io_cd = io_cd;
 		this.usr_id = usr_id;
+		this.process_id = process_id;
+		this.power = power;
 	}
 	public Work_Log() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	
 }
